8c26a2e0106339ca9cce1b7bc9610d09ab6108b6
Arthur Perrin <arthur.perrin1298@gmail.com>
2021-02-08 18:07:28 +0100

    first model created (1500 epochs)

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 
 # Model training parameters
 TRAIN_SPLIT = 0.8  # proportion of values to be used for training
-N_EPOCHS = 1 # number of epochs
+N_EPOCHS = 1500 # number of epochs
 BS = 32 # batch size
 
 N_FUTUR = 14  # Next days weather forecast
