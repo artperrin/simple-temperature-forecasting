c319f74f2f2fc5aa74c2c4dae193b9aee3bc257b
Arthur Perrin <arthur.perrin1298@gmail.com>
2021-02-19 08:53:41 +0100

    some changes

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 
 # Model training parameters
-N_EPOCHS = 200 # number of epochs
-BS = 32 # batch size
+N_EPOCHS = 750 # number of epochs
+BS = 16 # batch size
 
-N_FUTUR = 12  # Next (time unit) weather forecast
-N_PAST = 86  # Past (time unit) to compute the forecast
+N_FUTUR = 10  # Next (time unit) weather forecast
+N_PAST = 30  # Past (time unit) to compute the forecast
